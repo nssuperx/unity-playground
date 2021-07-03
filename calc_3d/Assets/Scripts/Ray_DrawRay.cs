f9fc436d6eae2a03ad7082517873310e40d5c345
nssuperx <40332687+nssuperx@users.noreply.github.com>
2021-07-03 19:36:28 +0900

    add: calc_3d

---
+++ calc_3d/Assets/Scripts/Ray_DrawRay.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ray_DrawRay : MonoBehaviour {
+    [SerializeField] private float distance = 100; // 飛ばす&表示するRayの長さ
+    [SerializeField] private float duration = 10; // 表示期間（秒）
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetMouseButtonDown(0)) {
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Debug.DrawRay(ray.origin, ray.direction * distance, Color.red, duration, false);
+
+            RaycastHit hit = new RaycastHit();
+            if (Physics.Raycast(ray, out hit, distance)) {
+                GameObject hitObject = hit.collider.gameObject;
+                string objectname = hitObject.GetComponent<SetSymbol>().GetSymbol();
+                Debug.Log(objectname);
+            }
+        }
+
+    }
+
+
+}
---
+++ calc_3d/Assets/Scripts/SetSymbol.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetSymbol : MonoBehaviour {
+    [SerializeField] private string symbol = "";
+
+    public string GetSymbol() {
+        return symbol;
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
