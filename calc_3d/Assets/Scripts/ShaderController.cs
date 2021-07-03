f36e04948af92f5f094154a07aa572259bbd313d
nssuperx <40332687+nssuperx@users.noreply.github.com>
2021-07-03 20:17:49 +0900

    update: calc_3d

---
+++ calc_3d/Assets/Scripts/ShaderController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShaderController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Renderer> ().material.SetColor ("_BaseColor", Color.red);
+    }
+}
